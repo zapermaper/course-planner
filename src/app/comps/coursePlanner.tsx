98f83b71ade3a9fc9ac8228f7a762b7c0b8bf793
Amanuel Tamrat <amanuel0625@gmail.com>
2025-02-21 12:08:08 -0600

    path change?

--- src/app/comps/coursePlanner.tsx
+++ src/app/comps/coursePlanner.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import PlanResult from './planResults';
+import PlanResult from 'src\app\comps\planResults.js';
 import { hsSubjects, apCourses, dualCreditCourses } from "./courseData";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
