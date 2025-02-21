4e61271250737d2645fe7567e5c250ebcafc11e3
Amanuel Tamrat <amanuel0625@gmail.com>
2025-02-21 12:32:02 -0600

    path 2.4

--- src/app/comps/coursePlanner.tsx
+++ src/app/comps/coursePlanner.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import planResult from './planResults';
+import planResults from './planResults';
 import { hsSubjects, apCourses, dualCreditCourses } from "./courseData";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -83,7 +83,7 @@ const CoursePlanner = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-100 to-yellow-50 p-4">
       {showResults ? (
-        <PlanResult 
+        <planResults 
           formData={formData}
           onBack={() => setShowResults(false)}
           hsSubjects={hsSubjects}
