c267b1c220b710f23c731916e1832544b999c9a3
Amanuel Tamrat <amanuel0625@gmail.com>
2025-02-24 02:59:59 -0600

    gitpages test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  output: "export",
+  basePath: "/course-planner", 
+  assetPrefix: "/course-planner/", 
+  images: {
+    unoptimized: true, 
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import CoursePlanner from './comps/coursePlanner'
+import CoursePlanner from './comps/coursePlanner';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <CoursePlanner />
+      <Image
+        src="/course-planner/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+        priority
+      />
+
     </main>
-  )
+  );
 }
